6fe2c8a4fc4b99a2d8a02990e25923acf4cd2abd
Nabil LARIBI <nabil.laribi@gmail.com>
2016-08-22 00:28:00 +0200

    clean dependencies

--- src/main/java/be/nabilo/MySpringBootProjectApplication.java
+++ src/main/java/be/nabilo/MySpringBootProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringBootProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootProjectApplication.class, args);
+    }
 }
--- src/test/java/be/nabilo/common/MyUtilsTest.java
+++ src/test/java/be/nabilo/common/MyUtilsTest.java
@@ -1,7 +1,5 @@
 package be.nabilo.common;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by Nabil on 21-08-16.
  */
